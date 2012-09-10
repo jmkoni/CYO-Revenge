82f45cdefccf059e91c3d63d83e471148fee8496
Jennifer M <jmorton429@gmail.com>
2012-09-10 13:51:45 -0500

    update revenge.py with destruct methods

--- revenge.py
+++ revenge.py
@@ -1,6 +1,40 @@
 from sys import exit
 import re
 
+def self_destruct():
+	print "It\'s 2002 and you are now an 18 year old billionaire with major issues."
+	print "You\'re partying like you just got out of juvie and maybe getting into a few bar fights."
+	print "Then pesky, pesky Nolan shows up again to bail you out."
+	print "He comments on your self-destruction and asks if you\'ve read the journals. What do you do?"
+	print " "
+	print "1) Say \"hell no, I don\'t care what they say. My father was a terrorist.\""
+	print "2) Consider that maybe this is not what you should be doing and maybe looking into your and your father\'s history would be worth your time."
+	next = raw_input("> ")
+	if next == "1":
+		destruct()
+	elif next == "2":
+		anditbegins()
+	else:
+		dead("Looks like the guy you were fighting with in the bar came back out and stabbed you.")
+def destruct():
+	print "It\'s New Year\'s 2003 and you pick the wrong bar to party at."
+	print "You start a fight with these two punks."
+	print "One is a larger woman who seems to have a knife."
+	print "The other is a skinny man who doesn?t appear to have a weapon."
+	print "Who do you attack first?"
+	print " "
+	print "1) Disarm the woman!"
+	print "2) Take out scrawny."
+	next = raw_input("> ")
+	if next == "1":
+		dead("Too bad, scrawny also had a knife! He stabs you in the back and you die.")
+	elif next == "2":
+		dead("Not quick enough! The woman stabs you as soon as you go for the man. You die.")
+	else:
+		dead("Too slow! The woman stabs you while the man punches you in the face. You die.")
+def anditbegins()
+	print "REVENGE TIME"
+
 def gold_room():
 	print "This room is full of gold. How much do you take?"
 
@@ -54,7 +88,7 @@ def cthulhu_room():
 		cthulhu_room()
 
 def dead(why):
-	print why, "Good job!"
+	print why, "Sorry."
 	exit(0)
 
 def start():
@@ -77,6 +111,6 @@ def start():
 	elif next == "2":
 		anditbegins()
 	else:
-		dead("You stumble around the room until you starve.")
+		dead("Looks like you drank too much tequila in Allenwood and forgot how to talk without slurring your words. You couldn't figure out how to get the money and end up homeless.")
 
 start() 
